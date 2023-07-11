f1fff4f1171f0a764c7d2579e193c0d893251c03
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-07-11 17:46:30 +0100

    feature 1.0: added color for the bottom tab navigation icons.

--- delivery_app_typescript/Navigator/TabNavigator.tsx
+++ delivery_app_typescript/Navigator/TabNavigator.tsx
@@ -1,10 +1,10 @@
-import { View, Text } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import CustomerScreen from "../Screens/CustomerScreen";
 import DeliveryMapScreen from "../Screens/DeliveryMapScreen";
 import LoginScreen from "../Screens/LoginScreen";
 import { useNavigation } from "@react-navigation/native";
+import { Icon } from "@rneui/themed";
 
 export type TabStackParamList = {
   Customers: undefined;
@@ -22,8 +22,33 @@ const TabNavigator = () => {
       headerShown: false,
     });
   }, []);
+
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: "#59C1CC",
+        tabBarInactiveTintColor: "gray",
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Customers") {
+            return (
+              <Icon
+                name="users"
+                type="entypo"
+                color={focused ? "##855ee6" : "#e6855e"}
+              />
+            );
+          } else if (route.name === "Orders") {
+            return (
+              <Icon
+                name="box"
+                type="entypo"
+                color={focused ? "##855ee6" : "#e6855e"}
+              />
+            );
+          }
+        },
+      })}
+    >
       <Tab.Screen name="Login" component={LoginScreen} />
       <Tab.Screen name="Customers" component={CustomerScreen} />
       <Tab.Screen name="Orders" component={DeliveryMapScreen} />
