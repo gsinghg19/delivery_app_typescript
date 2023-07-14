978b3a4dc1a64cd970dcaff2997ec5ac4ce87eff
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-07-14 15:51:17 +0100

    feature 4.1: added more type definitions to typings file

--- delivery_app_typescript/typings.d.ts
+++ delivery_app_typescript/typings.d.ts
@@ -5,5 +5,40 @@ type Customer = {
 
 type CustomerList = {
   name: ID;
-  Value: Customer;
+  value: Customer;
+};
+
+type Order = {
+  carrier: String;
+  createAt: Date;
+  shippingCost: Int;
+  trackingId: String;
+  Address: String;
+  City: String;
+  Lat: Float;
+  Lng: Float;
+  trackingItems: TrackingItems;
+};
+
+type OrderList = {
+  name: ID;
+  value: Order;
+};
+
+type Items = {
+  item_id: Int;
+  name: String;
+  price: Float;
+  quantity: Int;
+};
+
+type TrackingItems = {
+  customer_id: String;
+  items: [Items];
+  customer: Customer;
+};
+
+type TrackingItemsList = {
+  name: ID;
+  value: TrackingItems;
 };
