d659f12385897fa241d6a96c15cdd5edb6be6563
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-07-14 14:27:44 +0100

    feature 5.0: added Apollo client to app

--- delivery_app_typescript/App.tsx
+++ delivery_app_typescript/App.tsx
@@ -3,14 +3,27 @@ import { NavigationContainer } from "@react-navigation/native";
 import utilities from "./tailwind.json";
 import CustomerScreen from "./Screens/CustomerScreen";
 import RootNavigator from "./Navigator/RootNavigator";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://dashboard.stepzen.com/explorer?endpoint=api%2Fhopping-dragonfly",
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
     // @ts-ignore - TailwindProvider is missing a type defintion
     <TailwindProvider utilities={utilities}>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </ApolloProvider>
     </TailwindProvider>
   );
 }
