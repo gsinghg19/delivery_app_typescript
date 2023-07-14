55938c408e4bd28f15f89447ae5280f482fe6714
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-07-14 16:26:29 +0100

    fix 4.0: fixed error in the queries for Get_Orders

--- delivery_app_typescript/graphQL/Queries.tsx
+++ delivery_app_typescript/graphQL/Queries.tsx
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const GET_CUSTOMERS = gql`
-  query getCustomers {
+  query GetCustomers {
     getCustomers {
       name
       value {
@@ -13,27 +13,29 @@ export const GET_CUSTOMERS = gql`
 `;
 
 export const GET_ORDERS = gql`
-  query getOrders {
-    name
-    value {
-      Address
-      City
-      Lat
-      Lng
-      carrier
-      createAt
-      shippingCost
-      trackingId
-      trackingItems {
-        items {
-          item_id
-          name
-          price
-          quantity
-        }
-        customer {
-          email
-          name
+  query GetOrders {
+    getOrders {
+      name
+      value {
+        Address
+        City
+        Lat
+        Lng
+        carrier
+        createAt
+        shippingCost
+        trackingId
+        trackingItems {
+          items {
+            item_id
+            name
+            price
+            quantity
+          }
+          customer {
+            email
+            name
+          }
         }
       }
     }
