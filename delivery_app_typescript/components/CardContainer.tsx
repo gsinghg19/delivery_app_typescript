4ee805f0fa5bd16f99e15c701b4b2595b5a81bdf
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-07-19 16:39:31 +0100

    feature 8.2: added styling and aligment of card contents

--- delivery_app_typescript/components/CardContainer.tsx
+++ delivery_app_typescript/components/CardContainer.tsx
@@ -4,9 +4,9 @@ import useCustomerOrders from "../customHooks/useCustomerOrders";
 import { useTailwind } from "tailwind-rn/dist";
 import { useNavigation } from "@react-navigation/native";
 import { CustomerScrnCompositeNavProp } from "../Screens/CustomerScreen";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Card } from "@rneui/themed";
+import { Card, Icon } from "@rneui/themed";
 import ModalContainer from "./ModalContainer";
+import { CardDivider } from "@rneui/base/dist/Card/Card.Divider";
 
 type Props = {
   name: string;
@@ -22,26 +22,53 @@ const CardContainer = ({ email, userId, name }: Props) => {
   return (
     <TouchableOpacity>
       <Card containerStyle={tw("rounded-lg p-5")}>
-        <Text style={tw("text-2xl font-bold")}>
-          {loading ? <ActivityIndicator size="small" color="#0000ff" /> : name}
-        </Text>
-        <Text style={tw("text-sm")}>
-          ID:{" "}
+        <View>
+          <View style={tw("flex-row justify-between")}>
+            <View>
+              <Text style={tw("text-2xl font-bold")}>
+                {loading ? (
+                  <ActivityIndicator size="small" color="#0000ff" />
+                ) : (
+                  name
+                )}
+              </Text>
+              <Text style={[tw("text-sm"), { color: "#855ee6" }]}>
+                ID:{" "}
+                {loading ? (
+                  <ActivityIndicator size="small" color="#0000ff" />
+                ) : (
+                  userId
+                )}
+              </Text>
+            </View>
+
+            <View style={tw("flex-row justify-end items-center")}>
+              <Text>
+                {loading ? (
+                  <ActivityIndicator size="small" color="#0000ff" />
+                ) : (
+                  `${orders.length}x`
+                )}
+              </Text>
+              <Icon
+                name="box"
+                type="entypo"
+                style={tw("mb-5 ml-auto")}
+                color="#855ee6"
+                size={40}
+              />
+            </View>
+          </View>
+        </View>
+
+        <CardDivider />
+        <Text>
           {loading ? (
             <ActivityIndicator size="small" color="#0000ff" />
           ) : (
-            userId
+            `${email}`
           )}
         </Text>
-        <View>
-          <Text>
-            {loading ? (
-              <ActivityIndicator size="small" color="#0000ff" />
-            ) : (
-              `${orders.length}x`
-            )}
-          </Text>
-        </View>
       </Card>
     </TouchableOpacity>
   );
