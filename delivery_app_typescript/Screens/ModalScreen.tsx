3424a5ca4074896c7ae4a5892f1b5d40127df532
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-07-20 14:24:43 +0100

    feature 9.2: added a close button on the modal, so that the user can return to the customer screen.

--- delivery_app_typescript/Screens/ModalScreen.tsx
+++ delivery_app_typescript/Screens/ModalScreen.tsx
@@ -1,11 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { useTailwind } from "tailwind-rn/dist";
+import { Icon } from "@rneui/themed";
+import { TabStackParamList } from "../Navigator/TabNavigator";
+import { RootStackParamList } from "../Navigator/RootNavigator";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import {
+  useNavigation,
+  CompositeNavigationProp,
+} from "@react-navigation/native";
+
+type CustomerScrnCompositeNavProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList>,
+  NativeStackNavigationProp<RootStackParamList, "MyModal">
+>;
 
 const ModalScreen = () => {
+  const tw = useTailwind();
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>ModalScreen</Text>
-    </View>
+    <TouchableOpacity
+      style={tw("absolute top-0 right-0 pt-4 pr-4")}
+      onPress={navigation.goBack}
+    >
+      <Icon type="antdesign" name="closecircle" />
+    </TouchableOpacity>
   );
 };
 
